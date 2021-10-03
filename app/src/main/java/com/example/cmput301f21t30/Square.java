af4fe857e40876d40ecb1f452aee5d6c47415826
Harnoor Josan <harnoorvirjosan@gmail.com>
2021-10-02 22:31:23 -0600

    Corrected merge and added Square

--- app/src/main/java/com/example/cmput301f21t30/Square.java
+++ app/src/main/java/com/example/cmput301f21t30/Square.java
@@ -1,13 +1,8 @@
 package com.example.cmput301f21t30;
 
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.drawable.shapes.Shape;
-
-public class Square extends Shape {
-    @Override
-    public void draw(Canvas canvas, Paint paint) {
-
-
+public class Square extends Shape{
+    Square(){
+        
     }
+
 }
